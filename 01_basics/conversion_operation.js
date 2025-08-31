6a38253185fbe39606e7aca832238ddcfd03840e
bhoomitayal20 <145928057+bhoomitayal20@users.noreply.github.com>
2025-08-31 11:51:05 +0000

    conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,17 @@
+let score="33";
+console.log(Number(score));
+let name="bhoomi";
+console.log(typeof(Number(name)));
+console.log(Number(name));
+//"33"->33
+//33abc->NaN
+//true->1
+//false->0
+let isLogged=undefined;
+console.log(typeof Boolean(isLogged));
+console.log( Boolean(isLogged));
+//"Bhoomi"->true ,""->false,1->true,0->false,null->false
+let someNumber=33;
+console.log(typeof String(someNumber));
+console.log( String(someNumber));
+
