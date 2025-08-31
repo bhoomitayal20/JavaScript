3a3c475c870331e757c973ed5fbdb89c0b72cff4
bhoomitayal20 <145928057+bhoomitayal20@users.noreply.github.com>
2025-08-31 11:13:49 +0000

    datatypes understanding

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"//treat all java code as newer version
+//alert(3+3)//we are using node js not browser
+let name="Bhoomi";
+let age=20;
+let isHonest=true;
+let fav;
+let marks=null;
+console.table([typeof name,typeof age,typeof isHonest,typeof fav,typeof marks]);
