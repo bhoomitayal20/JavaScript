5eccfe3bd02466160dcb85f3747620af4ef02fed
bhoomitayal20 <145928057+bhoomitayal20@users.noreply.github.com>
2025-09-15 11:33:54 +0000

    truthy values

---
+++ 03_basics/truthy.js
@@ -0,0 +1,23 @@
+const userEmail='h@hitesh.ai'//if we add empty string here it is false but if we add empty array here it will be true why? AS EMPTY STRING IS FALSY VALUE
+if(userEmail){
+    console.log('got user email')
+    
+}
+else{
+    console.log('dont have useremail')
+}
+//truthy or falsy values
+//falsy values : false,0,-0,BigInt 0n,"",null,undefined,NaN
+//ELSE ALL ARE TRUTHY VALUES
+//"0",'fALSE'," ",[],{},function(){}->TRUTHY VALUE
+//how to check array is empty or not
+const user_array=[]
+if(user_array.length===0){
+    console.log("array is empty");
+    
+}
+const emptyObj={}
+if(Object.keys(emptyObj).length===0){
+    console.log("object is empty");
+    
+}
