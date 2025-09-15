01b83ef4f0b94bc6784cd58059ee63efbcb2d12f
bhoomitayal20 <145928057+bhoomitayal20@users.noreply.github.com>
2025-09-15 11:38:47 +0000

    nullish operator

--- 03_basics/truthy.js
+++ 03_basics/truthy.js
@@ -21,3 +21,12 @@ if(Object.keys(emptyObj).length===0){
     console.log("object is empty");
     
 }
+//mullish coalescing operator(??):null undefined
+let val1;
+//val1=5??10
+val1=null??10
+console.log(val1);
+let val2
+val2=undefined??15
+console.log(val2);
+
